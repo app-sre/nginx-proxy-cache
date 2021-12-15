f8dc2927955b89fde08a18e148508e92e3d43bcc
Maor Friedman <mafriedm@redhat.com>
2021-12-15 16:22:31 +0200

    update templating

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# if [[ -z "$REDIRECTOR_DESTINATION" ]]; then echo "Variable REDIRECTOR_DESTINATION not set. Aborting."; exit 1; fi
-# if [[ -z "$REDIRECTOR_TYPE" ]]; then echo "Variable REDIRECTOR_TYPE not set. Aborting."; exit 1; fi
-
-# if [[ "$REDIRECTOR_TYPE" != "redirect" ]] && [[ "$REDIRECTOR_TYPE" != "permanent" ]]; then
-#     echo "REDIRECTOR_TYPE should be set to either redirect or permanent. Aborting."
-#     exit 1;
-# fi
+if [[ -z "$PROXY_DESTINATION" ]]; then echo "Variable PROXY_DESTINATION not set. Aborting."; exit 1; fi
+if [[ -z "$PROXY_CACHE_VALID" ]]; then echo "Variable PROXY_CACHE_VALID not set. Aborting."; exit 1; fi
 
 # # Add trailing slash
-# REDIRECTOR_DESTINATION=${REDIRECTOR_DESTINATION%/} # Strip trailing slash if any
-# REDIRECTOR_DESTINATION="$REDIRECTOR_DESTINATION/"  # Add trailing slash
+REDIRECTOR_DESTINATION=${REDIRECTOR_DESTINATION%/} # Strip trailing slash if any
+REDIRECTOR_DESTINATION="$REDIRECTOR_DESTINATION/"  # Add trailing slash
 
-# echo "Configuring redirector to redirect all traffic to $REDIRECTOR_DESTINATION $REDIRECTOR_TYPE"
+echo "Configuring redirector to redirect all traffic to $REDIRECTOR_DESTINATION"
 /bin/cp -fv /nginx.conf.template /tmp/nginx.conf
-# sed -e "s@REDIRECTOR_DESTINATION@$REDIRECTOR_DESTINATION@g" -i /tmp/nginx.conf
-# sed -e "s@REDIRECTOR_TYPE@$REDIRECTOR_TYPE@g" -i /tmp/nginx.conf
+sed -e "s@REDIRECTOR_DESTINATION@$REDIRECTOR_DESTINATION@g" -i /tmp/nginx.conf
+sed -e "s@PROXY_CACHE_VALID@$PROXY_CACHE_VALID@g" -i /tmp/nginx.conf
 
 exec "$@"
