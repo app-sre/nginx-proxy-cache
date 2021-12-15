4caa57e59ae02753072301e85db736ec5bf90dde
Maor Friedman <mafriedm@redhat.com>
2021-12-15 16:28:00 +0200

    fix typos

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,12 @@ if [[ -z "$PROXY_DESTINATION" ]]; then echo "Variable PROXY_DESTINATION not set.
 if [[ -z "$PROXY_CACHE_VALID" ]]; then echo "Variable PROXY_CACHE_VALID not set. Aborting."; exit 1; fi
 
 # # Add trailing slash
-REDIRECTOR_DESTINATION=${REDIRECTOR_DESTINATION%/} # Strip trailing slash if any
-REDIRECTOR_DESTINATION="$REDIRECTOR_DESTINATION/"  # Add trailing slash
+PROXY_DESTINATION=${PROXY_DESTINATION%/} # Strip trailing slash if any
+PROXY_DESTINATION="$PROXY_DESTINATION/"  # Add trailing slash
 
-echo "Configuring redirector to redirect all traffic to $REDIRECTOR_DESTINATION"
+echo "Configuring proxy to cache all traffic to $PROXY_DESTINATION"
 /bin/cp -fv /nginx.conf.template /tmp/nginx.conf
-sed -e "s@REDIRECTOR_DESTINATION@$REDIRECTOR_DESTINATION@g" -i /tmp/nginx.conf
+sed -e "s@PROXY_DESTINATION@$PROXY_DESTINATION@g" -i /tmp/nginx.conf
 sed -e "s@PROXY_CACHE_VALID@$PROXY_CACHE_VALID@g" -i /tmp/nginx.conf
 
 exec "$@"
